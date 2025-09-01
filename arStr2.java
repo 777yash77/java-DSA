c9c882d24093cc9718ae12a0746378a0739e3b06
ywanth224 <ywanth224@gmail.com>
2025-09-01 09:24:19 +0000

    formatted

--- arStr2.java
+++ arStr2.java
@@ -4,24 +4,22 @@ import java.util.Scanner;
 public class arStr2 {
     
         public static void main(String[]args){
-               
-                       
-                               Scanner sc=new Scanner(System.in);
-                                       int size=sc.nextInt();
-                                               int limiter=sc.nextInt();
-                                                       int[]arr=new int[size];
-                                                               int k=0;
-                                                                    for(int i=limiter;i<size;i++){
-                                                                                 arr[k]=i;
-                                                                                              k++;
-                                                                                                   }
-                                                                                                        k=0;
-                                                                                                                for(int i=(size-limiter);i<size;i++){
-                                                                                                                            arr[i]=k;
-                                                                                                                                        k++;
-                                                                                                                                                }
-                                                                                                                                                         for(int i=0;i<size;i++){
-                                                                                                                                                                     System.out.print(arr[i]+" ");
-                                                                                                                                                                             }
-                                                                                                                                                                                 }
-                                                                                                                                                                                 }
+        Scanner sc=new Scanner(System.in);
+        int size=sc.nextInt();
+        int limiter=sc.nextInt();
+        int[]arr=new int[size];
+        int k=0;
+        for(int i=limiter;i<size;i++){
+        arr[k]=i;
+     k++;
+ }
+      k=0;
+      for(int i=(size-limiter);i<size;i++){
+      arr[i]=k;
+         k++;
+                }
+   for(int i=0;i<size;i++){
+      System.out.print(arr[i]+" ");
+          }
+                                   }
+                                             }
